9bf5503986e66627c9f0d8744778cc2f30f3a160
Wajahat Karim <wajahatkarim3@gmil.com>
2016-12-24 13:39:55 +0500

    another drawable test added!!!

---
+++ CirleProgress-Sample/wajiprogressdrawable/src/main/java/com/wajahatkarim/wajiprogressdrawable/views/AnotherProgressDrawable.java
@@ -0,0 +1,12 @@
+package com.wajahatkarim.wajiprogressdrawable.views;
+
+import com.facebook.drawee.drawable.ProgressBarDrawable;
+
+/**
+ * Created by Wajahat Karim on 12/24/2016.
+ */
+
+public class AnotherProgressDrawable extends ProgressBarDrawable {
+
+
+}
